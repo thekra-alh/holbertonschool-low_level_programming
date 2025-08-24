71b58d78aca5b0aea4eea14554ee01ea0ce2e451
thekra-alh <202222146@yu.sa.com>
2025-08-24 16:27:19 +0000

    update

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * My function
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* My function
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
  puts("\"Programming is like building a multilingual puzzle");

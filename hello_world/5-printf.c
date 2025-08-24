b48a7577dd09412b255228990ce2727c379fa4f6
thekra-alh <202222146@yu.sa.com>
2025-08-24 16:33:19 +0000

    k

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints a specific message using printf
- *
- * Return: Always 0 (Success)
- */
+* main - Prints a specific message using printf
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

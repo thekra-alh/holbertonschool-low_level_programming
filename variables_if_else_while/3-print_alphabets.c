da6d7dbc8da93414cb84df4aca2d71d69c22f0c4
thekra-alh <202222146@yu.sa.com>
2025-08-25 21:15:11 +0000

    l

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Description: Prints the alphabet in lowercase,
- * then in uppercase using putchar
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: Prints the alphabet in lowercase,
+* then in uppercase using putchar
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-        char lower;
-        char upper;
-
-        for (lower = 'a'; lower <= 'z'; lower++)
-                putchar(lower);
-
-        for (upper = 'A'; upper <= 'Z'; upper++)
-                putchar(upper);
-
-        putchar('\n');
-
-        return (0);
+char lower
+char upper;
+for (lower = 'a'; lower <= 'z'; lower++)
+putchar(lower);
+for (upper = 'A'; upper <= 'Z'; upper++)
+putchar(upper);
+putchar('\n');
+return (0);
 }

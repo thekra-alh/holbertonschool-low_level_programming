db025d61e10df1653a4d19809b90f003ffedb9fa
thekra-alh <202222146@yu.sa.com>
2025-08-25 16:38:22 +0000

    l

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+
 /**
-* main - Entry point
-*
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char ch;
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+    return (0);
+}

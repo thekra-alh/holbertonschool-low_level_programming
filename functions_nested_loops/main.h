9d80985cc047ef016deb9e1e5a3af01f19507f78
thekra-alh <202222146@yu.sa.com>
2025-08-27 15:03:10 +0000

    k

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _putchar(char c);
+int _isalpha(int c);
+int _putchar(char c);
+int print_sign(int n);
+int _putchar(char c);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _putchar(char c);
+void print_times_table(int n);
+int _putchar(char c);
+void natural_sum(void);
+
+#endif

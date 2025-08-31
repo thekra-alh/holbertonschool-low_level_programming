ca2137b45d248c7adc53b2b284189710017eefd8
thekra-alh <202222146@yu.sa.com>
2025-08-31 14:41:18 +0000

    l

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: the character to check (ASCII code)
- *
- * Return: 1 if c is uppercase, 0 otherwise
- */
+* _isupper - checks for uppercase character
+* @c: the character to check (ASCII code)
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    else
-        return (0);
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
 }

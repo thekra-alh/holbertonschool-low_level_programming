40c47fea0693d7751fc28faaedd1ef55bc95fd91
thekra-alh <202222146@yu.sa.com>
2025-09-01 14:15:55 +0000

    l

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
+ * _puts - prints a string, followed by a new line
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 void reset_to_98(int *n);  /* <- This line is critical */
+void _puts(char *str)
 
 #endif /* MAIN_H */

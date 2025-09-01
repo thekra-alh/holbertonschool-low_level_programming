42f6f8299082ddf78ebd904a7abeeef1f59118f7
thekra-alh <202222146@yu.sa.com>
2025-09-01 12:48:50 +0000

    l

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* main - check the code 
-*
-* Return: Always 0.
-*/
-int main(void)
+  * reset_to_98 - Updates the value it points to 98
+  * @n: pointer to n
+  *
+  * Return: void
+  */
+void reset_to_98(int *n)
 {
-int n;
-
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
+	*n = 98;
 }

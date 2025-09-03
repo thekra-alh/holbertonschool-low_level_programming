23038a5589b6761fb7f6b8521b71c6ec1546e45d
thekra-alh <202222146@yu.sa.com>
2025-09-03 13:30:21 +0000

    l

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,33 @@
-`#include "main.h"
+#include "main.h"
 
 /**
- * _strncpy - copies a string
- * @dest: buffer storing the string copy
- * @src: source string
- * @n: maximum number of bytes to copy
- *
- * Description: Custom implementation of the strncpy behavior
- *              from the C standard library.
- *
- * Return: pointer to dest
- */
+* _strncpy - Copy a string
+* @dest: The destination value
+* @src: The source value
+* @n: The copy limit
+*
+* Return: char value
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int a = 0, b = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	for (; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
+while (src[b])
+{
+b++;
+}
 
-	return (dest);
+while (a < n && src[a])
+{
+dest[a] = src[a];
+a++;
+}
+
+while (a < n)
+{
+dest[a] = '\0';
+a++;
+}
+
+return (dest);
 }

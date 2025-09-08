c20404b44a5723cda5da70c4ba4d80b44208cafe
thekra-alh <202222146@yu.sa.com>
2025-09-08 13:26:47 +0000

    l

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: the base
+ * @y: the exponent
+ *
+ * Return: x^y, or -1 if y < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0) /* error case */
+		return (-1);
+	if (y == 0) /* base case: any number to power 0 is 1 */
+		return (1);
+	return (x * _pow_recursion(x, y - 1)); /* recursive step */
+}

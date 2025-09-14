70444781236b11ab024df0c4dcc262f6ddddd53b
thekra-alh <202222146@yu.sa.com>
2025-09-14 16:29:12 +0000

    l

---
+++ more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar function prototype (if needed) */
+int _putchar(char c);
+
+/* 0-malloc_checked.c */
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif /* MAIN_H */

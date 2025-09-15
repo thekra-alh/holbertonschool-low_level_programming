f32065bb88e8760e4f1e077c74caefa8a8a1930f
thekra-alh <202222146@yu.sa.com>
2025-09-15 21:25:00 +0000

    l

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - defines a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
+* struct dog - defines a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/
 struct dog
 {
     char *name;

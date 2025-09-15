599836c16abbec05cc084ec13688696f6088bbd0
thekra-alh <202222146@yu.sa.com>
2025-09-15 21:37:47 +0000

    l

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+* init_dog - Initializes a variable of type struct dog
+* @d: pointer to struct dog to initialize
+* @name: name to assign
+* @age: age to assign
+* @owner: owner to assign
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

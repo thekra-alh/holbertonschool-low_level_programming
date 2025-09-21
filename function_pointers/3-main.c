bfea87526a609d646a4b9e431f976528a89f1f11
thekra-alh <202222146@yu.sa.com>
2025-09-21 18:56:26 +0000

    l

---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point for the calculator
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success, exits with error code otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = operation(a, b);
+	printf("%d\n", result);
+
+	return (0);
+}

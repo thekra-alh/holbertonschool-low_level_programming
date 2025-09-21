41b17e80e5b97adad179468934e48db949edc4e1
thekra-alh <202222146@yu.sa.com>
2025-09-21 18:27:30 +0000

    l

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stddef.h>  // For NULL
+
+/**
+* print_name - Prints a name using a function pointer.
+* @name: The name to be printed.
+* @f: A function pointer that takes a char * and returns void.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+    if (name != NULL && f != NULL)
+    {
+        f(name);
+    }
+}

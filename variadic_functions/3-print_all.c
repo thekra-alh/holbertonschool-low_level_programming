cd5cc26650c2f1273b337178c9b692d0ce58decf
thekra-alh <202222146@yu.sa.com>
2025-09-24 19:57:14 +0000

    l

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,42 +1,38 @@
-#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 
 /**
-* print_all - Prints anything based on format
-* @format: A list of types of arguments passed to the function
-*
-* Return: void
+* print_all - Prints anything, based on format string
+* @format: List of types of arguments passed to the function
 */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0, j = 0;
-	char *str, *sep = "";
-	char fmts[] = {'c', 'i', 'f', 's'};
-	void (*printers[4])(va_list) = {
-		[](va_list a) { printf("%c", va_arg(a, int)); },
-		[](va_list a) { printf("%d", va_arg(a, int)); },
-		[](va_list a) { printf("%f", va_arg(a, double)); },
-		[](va_list a)
-		{
-			str = va_arg(a, char *);
-			printf("%s", str ? str : "(nil)");
-		}
-	};
+	int i = 0;
+	char *str;
+	char *sep = "";
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		j = 0;
-		while (j < 4 && format[i] != fmts[j])
-			j++;
-
-		if (j < 4)
+		if (format[i] == 'c' || format[i] == 'i' ||
+		    format[i] == 'f' || format[i] == 's')
 		{
 			printf("%s", sep);
-			printers[j](args);
+			if (format[i] == 'c')
+				printf("%c", va_arg(args, int));
+			if (format[i] == 'i')
+				printf("%d", va_arg(args, int));
+			if (format[i] == 'f')
+				printf("%f", va_arg(args, double));
+			if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+			}
 			sep = ", ";
 		}
 		i++;

ee2c44d51cf158a6b9b8e33060da4f40b73587ee
thekra-alh <202222146@yu.sa.com>
2025-09-24 20:07:50 +0000

    l

--- variadic_functions/3-main.c
+++ variadic_functions/3-main.c
@@ -1,12 +1,17 @@
 #include "variadic_functions.h"
 
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	print_all("ceis", 'B', 3, "stSchool");
+	print_all("fcsi", 3.14435, 'H', "#Cisfun", 0);
+	print_all("mnbvfqcepolsbxzi", 3.14435, 'H', "#Cisfun", 0);
+	print_all("qwertyuopadghjklzxvbnm");
+	print_all("");
+	print_all(NULL);
+	print_all("fcsi", 3.14435, 'H', NULL, 402);
 	return (0);
 }
--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,43 +1,51 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 
 /**
 * print_all - Prints anything, based on format string
 * @format: List of types of arguments passed to the function
+*
+* Return: 0 (SUCCESS)
 */
-void print_all(const char * const format, ...)
+int print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0;
-	char *str;
-	char *sep = "";
+	int i = 0, j;
+	char *str, *sep = "";
+	char types[] = {'c', 'i', 'f', 's'};
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c' || format[i] == 'i' ||
-		    format[i] == 'f' || format[i] == 's')
+		j = 0;
+		while (j < 4)
 		{
-			printf("%s", sep);
-			if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-			if (format[i] == 'i')
-				printf("%d", va_arg(args, int));
-			if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-			if (format[i] == 's')
+			if (format[i] == types[j])
 			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s", str);
+				printf("%s", sep);
+				if (j == 0)
+					printf("%c", va_arg(args, int));
+				if (j == 1)
+					printf("%d", va_arg(args, int));
+				if (j == 2)
+					printf("%f", va_arg(args, double));
+				if (j == 3)
+				{
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s", str);
+				}
+				sep = ", ";
 			}
-			sep = ", ";
+			j++;
 		}
 		i++;
 	}
 
 	va_end(args);
 	printf("\n");
+	return (0);
 }

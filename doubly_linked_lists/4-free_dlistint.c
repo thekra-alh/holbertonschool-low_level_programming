f1c29cbda95dbe6c82cd18dad3d5631d18517efe
thekra-alh <202222146@yu.sa.com>
2025-10-05 14:36:40 +0000

    l

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
-* free_dlistint - frees a dlistint_t list
+* free_dlistint - frees a dlistint_t doubly linked list
 * @head: pointer to the head of the list
 */
 void free_dlistint(dlistint_t *head)

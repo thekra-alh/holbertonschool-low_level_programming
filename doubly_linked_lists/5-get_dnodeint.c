8d4c77f7c07d6a6a3e564fc2971facba51f24fb6
thekra-alh <202222146@yu.sa.com>
2025-10-05 15:03:35 +0000

    l

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,8 @@
 /**
 * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
 * @head: pointer to the head of the list
-* @index: index of the node, starting from 0 
+* @index: index of the node, starting from 0
+*
 * Return: pointer to the node at index, or NULL if it does not exist
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
@@ -19,4 +20,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	}
 
 	return (NULL);
+:%s/\s\+$//e
 }

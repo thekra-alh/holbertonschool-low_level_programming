0a704ee0c8cf454391e60abddc6c35d9dc0d4027
thekra-alh <202222146@yu.sa.com>
2025-10-12 15:14:11 +0000

    l

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,16 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer
+ * struct dlistint_s - doubly linked list node
+ * @n: integer stored in the node
  * @prev: points to the previous node
  * @next: points to the next node
- *
- * Description: doubly linked list node structure
  */
 typedef struct dlistint_s
 {
@@ -19,13 +16,9 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 void free_dlistint(dlistint_t *head);
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+size_t print_dlistint(const dlistint_t *h);
+/* other function prototypes if needed */
 
 #endif /* LISTS_H */

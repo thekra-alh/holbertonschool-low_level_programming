4fbd91f198e9168acaa60c86bc3f2749f9468003
thekra-alh <202222146@yu.sa.com>
2025-10-12 16:00:30 +0000

    l

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,11 +12,11 @@
  */
 static void close_or_die(int fd)
 {
-    if (close(fd) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-        exit(100);
-    }
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 
 /**
@@ -29,18 +29,18 @@ static void close_or_die(int fd)
  */
 static ssize_t read_retry(int fd, char *buf, size_t n, const char *name)
 {
-    ssize_t r;
+	ssize_t r;
 
-    do {
-        r = read(fd, buf, n);
-    } while (r == -1 && errno == EINTR);
+	do {
+		r = read(fd, buf, n);
+	} while (r == -1 && errno == EINTR);
 
-    if (r == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
-        exit(98);
-    }
-    return (r);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+		exit(98);
+	}
+	return (r);
 }
 
 /**
@@ -52,21 +52,21 @@ static ssize_t read_retry(int fd, char *buf, size_t n, const char *name)
  */
 static void write_all(int fd, const char *name, const char *buf, ssize_t n)
 {
-    ssize_t off = 0, w;
+	ssize_t off = 0, w;
 
-    while (off < n)
-    {
-        do {
-            w = write(fd, buf + off, n - off);
-        } while (w == -1 && errno == EINTR);
+	while (off < n)
+	{
+		do {
+			w = write(fd, buf + off, n - off);
+		} while (w == -1 && errno == EINTR);
 
-        if (w == -1)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
-            exit(99);
-        }
-        off += w;
-    }
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
+			exit(99);
+		}
+		off += w;
+	}
 }
 
 /**
@@ -77,35 +77,35 @@ static void write_all(int fd, const char *name, const char *buf, ssize_t n)
  */
 int main(int ac, char **av)
 {
-    int f_from, f_to;
-    ssize_t r;
-    char buf[BUF];
+	int f_from, f_to;
+	ssize_t r;
+	char buf[BUF];
 
-    if (ac != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-    f_from = open(av[1], O_RDONLY);
-    if (f_from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-        exit(98);
-    }
+	f_from = open(av[1], O_RDONLY);
+	if (f_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
-    f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (f_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-        close_or_die(f_from);
-        exit(99);
-    }
+	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (f_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close_or_die(f_from);
+		exit(99);
+	}
 
-    while ((r = read_retry(f_from, buf, BUF, av[1])) > 0)
-        write_all(f_to, av[2], buf, r);
+	while ((r = read_retry(f_from, buf, BUF, av[1])) > 0)
+		write_all(f_to, av[2], buf, r);
 
-    close_or_die(f_from);
-    close_or_die(f_to);
-    return (0);
+	close_or_die(f_from);
+	close_or_die(f_to);
+	return (0);
 }

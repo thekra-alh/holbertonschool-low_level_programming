089b89bcdceb7a3bca31e11163e8df80765a2792
thekra-alh <202222146@yu.sa.com>
2025-10-21 15:30:55 +0000

    l

--- hash_tables/0-main.c
+++ hash_tables/0-main.c
@@ -2,11 +2,16 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(void)
 {
     hash_table_t *ht;
 
-    ht = hash_table_create(1024);
+    ht = hash_table_create(2048);
     printf("%p\n", (void *)ht);
     return (EXIT_SUCCESS);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -36,4 +36,3 @@ void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
-
